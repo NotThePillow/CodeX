1e195eb5cce04dc47e2dc82e6af582a7b65e67f3
NotThePillow <98300630+NotThePillow@users.noreply.github.com>
2022-01-25 09:35:28 +0400

    fixing python file

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from distutils.log import debug
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,9 +12,6 @@ def index():
 def test():
     return render_template('test.html')
 
-@app.route('/hello/<name>')
-def hello(name):
-    return render_template('page.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
